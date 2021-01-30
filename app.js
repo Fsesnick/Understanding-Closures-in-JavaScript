83b1333957c575ef7f43449759ca9eb2502fc724
Fsesnick <fernandasesnick@gmail.com>
2021-01-30 17:45:37 +0000

    final

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 var count = 0;
 
+var dogCount = 0;
+var birdCount = 0;
+
 function countBirds() {
   count += 1;
   return count + ' birds';
+}
+
+function countDogs() {
+  count += 1;
+  return count + ' dogs';
 }
